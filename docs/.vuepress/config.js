cb78e668a8e96ce73ec3c48ab64e7961c97ab00e
zeng-zhe <Zeng_Zhe@outlook.com>
2020-09-01 17:44:47 +0800

    ✨feat(docs): 新增一个模板，调整config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,22 @@
 module.exports = {
-  title: 'Gorvey的blog',  // 设置网站标题
-  dest: 'public',    // 设置输出目录
+  title: 'Gorvey的blog', // 设置网站标题
+  dest: 'public', // 设置输出目录
   base: '/', // 设置站点根路径
   theme: 'reco',
-  repo: 'https://github.com/Zeng-Zhe',// 添加 github 链接
+  repo: 'https://github.com/Zeng-Zhe', // 添加 github 链接
   evergreen: true, //禁止兼容低版本浏览器
   themeConfig: {
     type: 'blog',
+    lastUpdated: 'Last Updated', // string | boolean
+    displayAllHeaders: true,
+    sidebar: 'auto',
     nav: [
       { text: 'Home', link: '/' },
-      { text: '更多', link: '/more/' },
-      { text: 'External', link: 'https://google.com' },
-    ]
-  }
-}
+      { text: '笔记', link: '/notes/mark' },
+      { text: '关于我', link: '/about/' },
+    ],
+    sidebar: {
+      '/notes/': ['/notes/mark', '/notes/算法和数据结构'],
+    },
+  },
+};
---
+++ docs/about/README.md
@@ -0,0 +1,8 @@
+---
+title: 关于我
+date: 2020-9-1
+sidebar: 'auto'
+publish: false
+---
+
+# 关于本站
