fdf5ffd9449067264eae378791b5442bf38a72a6
zeng-zhe <Zeng_Zhe@outlook.com>
2020-09-08 09:22:44 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,16 @@
 module.exports = {
   title: 'Gorvey的blog', // 设置网站标题
+  description: 'Web前端技术博客，积跬步以至千里，致敬每个爱学习的你。',
   dest: 'public', // 设置输出目录
   base: '/', // 设置站点根路径
   theme: 'reco',
   repo: 'https://github.com/Zeng-Zhe', // 添加 github 链接
   evergreen: true, //禁止兼容低版本浏览器
-
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+    },
+  },
   // plugins: [
   //   [
   //     'vuepress-plugin-comment',
@@ -26,10 +31,15 @@ module.exports = {
     type: 'blog',
     lastUpdated: '最后更新时间', // string | boolean
     displayAllHeaders: true,
-
+    search: true,
+    searchMaxSuggestions: 10,
     sidebarDepth: 1,
+    // 作者
+    author: 'gorvey',
     nav: [
-      { text: 'Home', link: '/' },
+      { text: '首页', link: '/' },
+      { text: '前端', link: '/frontend/' },
+
       {
         text: '收藏',
         icon: 'reco-document',
@@ -38,7 +48,13 @@ module.exports = {
           { text: '工具', link: '/mark/工具' },
         ],
       },
-      { text: '关于我', link: '/about/' },
+      { text: '留言板', link: '/messageBoard/', icon: 'reco-suggestion' },
+      { text: '关于我', link: '/about/', icon: 'reco-account' },
+      {
+        text: '更多',
+        link: '/more/',
+        items: [{ text: '面试', link: '/more/面试' }],
+      },
     ],
     sidebar: {
       // '/notes/': ['/notes/mark', '/notes/算法和数据结构'],
@@ -47,17 +63,17 @@ module.exports = {
     subSidebar: 'auto',
     sidebarDepth: 4,
     startYear: '2020',
-    // 博客配置
-    blogConfig: {
-      category: {
-        location: 2, // 在导航栏菜单中所占的位置，默认2
-        text: '分类', // 默认文案 “分类”
-      },
-      tag: {
-        location: 3, // 在导航栏菜单中所占的位置，默认3
-        text: '标签', // 默认文案 “标签”
-      },
-    },
+    // // 博客配置
+    // blogConfig: {
+    //   category: {
+    //     location: 2, // 在导航栏菜单中所占的位置，默认2
+    //     text: '分类', // 默认文案 “分类”
+    //   },
+    //   tag: {
+    //     location: 3, // 在导航栏菜单中所占的位置，默认3
+    //     text: '标签', // 默认文案 “标签”
+    //   },
+    // },
     valineConfig: {
       appId: 'ljDhFCYbJztk72HHbFSg6rzY-gzGzoHsz', // your appId
       appKey: '4Ibfb6Dk0qHG8JlpvjTFttBc', // your appKey
