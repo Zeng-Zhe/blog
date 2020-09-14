7807de1389aca6314f9bdf449a5b615c0cdc7b1f
zeng-zhe <Zeng_Zhe@outlook.com>
2020-09-14 16:49:38 +0800

    update

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,18 +1,47 @@
 module.exports = [
   { text: '首页', link: '/' },
-  { text: '前端', link: '/note/fe/' },
+  {
+    text: '前端',
+    link: '/note/fe/',
+    items: [
+      { text: '建立自己的知识体系', link: '/pages/b315f9/' },
+      {
+        text: '知识体系',
+        link: '/pages/6fa45d/',
+        items: [{ text: 'JavaScript进阶', link: '/pages/7264d1/' }],
+      },
+    ],
+  },
 
   {
     text: '收藏',
     items: [
-      { text: '文章', link: '/mark/文章' },
-      { text: '工具', link: '/mark/工具' },
+      { text: '文章', link: '/pages/af1346/' },
+      { text: '工具', link: '/pages/47fdcc/' },
+      { text: '网站', link: '/pages/d9f9a3/' },
     ],
   },
   {
     text: '更多',
     items: [{ text: '博客建设', link: '/pages/16e149/' }],
   },
-  { text: '留言板', link: '/messageBoard/', icon: 'reco-suggestion' },
-  { text: '关于我', link: '/about/', icon: 'reco-account' },
+  {
+    text: '索引',
+    items: [
+      {
+        text: '分类',
+        link: '/categories/',
+      },
+      {
+        text: '标签',
+        link: '/tags/',
+      },
+      {
+        text: '归档',
+        link: '/archives/',
+      },
+    ],
+  },
+  { text: '留言板', link: '/messageBoard/' },
+  { text: '关于', link: '/about/' },
 ];
