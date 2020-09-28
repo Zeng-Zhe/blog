0c67d0908ad8b6d1396ae34c7b02579cf2975742
Gorvey <Zeng_Zhe@outlook.com>
2020-09-28 18:04:25 +0800

    init

---
+++ codeSnippets/js/new 操作符.js
@@ -0,0 +1,37 @@
+//#region 01
+function Test() {
+  this.name = 'Gorvey';
+
+  Test.prototype.sayName = function() {
+    console.log(this.name);
+  };
+}
+
+let person = new Test();
+console.log(person.name); //Gorvey
+person.sayName(); // Gorvey
+//#endregion 01
+let variable = null;
+//#region 02
+function Test() {
+  this.name = 'Gorvey';
+
+  Test.prototype.sayName = function() {
+    console.log(this.name);
+  };
+
+  return { name: 'abc' };
+}
+
+let person = new Test();
+console.log(person.name); //abc
+person.sayName(); // TypeError
+//#endregion 02
+
+//#region 03
+function New() {
+  //创建对象
+  let obj = {};
+  //设置__proto__
+}
+//#endregion 03
--- codeSnippets/红宝书/原型.js
+++ codeSnippets/红宝书/原型.js
@@ -1,5 +1,5 @@
 //#region 01
-//声明一个函数
+//声明一个构造函数，按照约定，首字母大写
 function Person() {}
 //创建完成后，就会有一个原型对象
 console.log(Person.prototype);
@@ -11,14 +11,28 @@ console.log(Person.prototype);
 
 // 默认只会有constructor属性，指向构造函数。这里指向了Person
 console.log(Person === Person.prototype.constructor); // true
-let person1 = new Person();
-//每个对象都拥有__proto__。用于获取原型
-console.log(person1.__proto__ === Person.prototype);
 //#endregion 01
 
 //#region 02
+let person1 = new Person();
+//每个对象都拥有__proto__。用于获取原型
+console.log(person1.__proto__ === Person.prototype); //true
+//#endregion 02
+//#region 03
 function Car() {}
 Car.prototype.name = 'abc';
 let car = new Car();
 console.log(car.name); //abc
-//#endregion 02
+//#endregion 03
+
+//#region 04
+let obj = new Object();
+console.log(obj.__proto__ === Object.prototype); //true
+
+//Person.prototype也是对象，由Object构造，所以指向Object.prototype
+console.log(Person.prototype.__proto__ === Object.prototype); //true
+//#endregion 04
+
+//#region 05
+console.log(Object.prototype.__proto__ === null); //true
+//#endregion 05
