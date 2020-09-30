f55b648b14c2bfd77d412b5d5a255bdd6b92572d
Gorvey <Zeng_Zhe@outlook.com>
2020-09-30 16:37:12 +0800

    update

--- codeSnippets/js/作用域.js
+++ codeSnippets/js/作用域.js
@@ -12,3 +12,19 @@ function bar() {
 
 bar();
 //#endregion 01
+
+//#region 02
+let name = 'Gorvey';
+
+function first() {
+  console.log(name);
+  second();
+}
+
+function second() {
+  console.log('i am second func');
+}
+
+first();
+
+//#endregion 02
