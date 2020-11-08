874d3e73007313a3611e7bfec9bacd5ccf3f43d4
Gorvey <Zeng-Zhe@outlook.com>
2020-11-08 22:56:42 +0800

    update

---
+++ codeSnippets/promise.js
@@ -0,0 +1,18 @@
+export class Promise {
+  constructor(fn) {
+
+    try {
+      fn(this.resolve, this.reject)
+    } catch (e) {
+      this.reject(e)
+    }
+  }
+  resolve() {
+
+  }
+
+
+}
+Promise.prototype.then = function (onResolved) {
+  
+}
